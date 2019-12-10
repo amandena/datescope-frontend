0b0c0665154ee633ee10497c0702f08fd93b8c78
amandena <manda.panda91@yahoo.com>
2019-12-10 17:53:45 -0500

    added handleChange to Login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -7,15 +7,21 @@ class Login extends React.Component {
     password: ''
   }
 
+  handleChange = event => {
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return(
       <div>
         Welcome Back!
         <form>
           <label>Email: </label>
-          <input type='text' placeholder='Email' name='email' /><br/>
+          <input type='text' placeholder='Email' name='email' value={this.state.email} onChange={this.handleChange} /><br/>
           <label>Password: </label>
-          <input type='password' placeholder='Password' name='password' /><br/>
+          <input type='password' placeholder='Password' name='password' value={this.state.password} onChange={this.handleChange} /><br/>
           <input type='submit' value='Login' />
         </form>
       </div>
