9b01299382458067d1ede7bbdea0260f1d97f02c
amandena <manda.panda91@yahoo.com>
2020-01-15 23:28:30 -0500

    connected loginUser and implemented handleSubmit in form for Login component

--- src/actions/loginUser.js
+++ src/actions/loginUser.js
@@ -1,4 +1,5 @@
 export const loginUser = credentials => {
+  console.log('credentials are', credentials)
   return (dispatch) => {
     fetch('http://localhost:3000/api/v1/login', {
       credentials: 'include',
--- src/components/Login.js
+++ src/components/Login.js
@@ -2,36 +2,30 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {updateLoginForm} from '../actions/updateLoginForm'
-// import {loginUser} from '../actions/loginUser'
+import {loginUser} from '../actions/loginUser'
 
-const Login = ({loginForm, updateLoginForm}) => {
+const Login = ({loginFormData, updateLoginForm, loginUser}) => {
   const handleChange = event => {
     const {name, value} = event.target
     const updatedFormInfo = {
-      ...loginForm,
+      ...loginFormData,
       [name]: value
     }
     updateLoginForm(updatedFormInfo)
   }
 
-  // handleSubmit = event => {
-  //   event.preventDefault()
-  //   this.props.loginUser(this.state)
-  //   this.setState({
-  //     loginForm: {
-  //       email: '',
-  //       password: ''
-  //     }
-  //   })
-  // }
+  const handleSubmit = event => {
+    event.preventDefault()
+    loginUser(loginFormData)
+  }
 
 
   return(
     <div>
       <h2>Welcome Back!</h2>
-      <form >
-        <input type='text' placeholder='Email' name='email' value={loginForm.email} onChange={handleChange} /><br/>
-        <input type='password' placeholder='Password' name='password' value={loginForm.password} onChange={handleChange} /><br/>
+      <form onSubmit={handleSubmit}>
+        <input type='text' placeholder='Email' name='email' value={loginFormData.email} onChange={handleChange} /><br/>
+        <input type='password' placeholder='Password' name='password' value={loginFormData.password} onChange={handleChange} /><br/>
         <input type='submit' value='Login' />
       </form>
       <p>New to Datescope? Create account <Link to='/users/new'>here</Link>.</p>
@@ -41,8 +35,8 @@ const Login = ({loginForm, updateLoginForm}) => {
 
 const mapStateToProps = state => {
   return {
-    loginForm: state.loginForm
+    loginFormData: state.loginForm
   }
 }
 
-export default connect(mapStateToProps, {updateLoginForm})(Login)
+export default connect(mapStateToProps, {updateLoginForm, loginUser})(Login)
