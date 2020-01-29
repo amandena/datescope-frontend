ad9f265b8ee2f0cd2a8fd9373abb6028234bf172
amandena <manda.panda91@yahoo.com>
2020-01-28 21:53:35 -0500

    added initialState

--- src/reducers/signupForm.js
+++ src/reducers/signupForm.js
@@ -1,4 +1,17 @@
-export default (state = {}, action) => {
+const initialState = {
+  email: '',
+  password: '',
+  name: '',
+  age: '',
+  sign: '',
+  bio: '',
+  gender: '',
+  preference: '',
+  location: '',
+  instagram: ''
+}
+
+export default (state = initialState, action) => {
   switch (action.type) {
     default:
       return state
