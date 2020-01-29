ef02d171956087e47627584834af2cd02cced56c
amandena <manda.panda91@yahoo.com>
2020-01-29 00:40:22 -0500

    refactored signup action and now new users can be added/signup properlygit add .

--- src/actions/signup.js
+++ src/actions/signup.js
@@ -1,19 +1,24 @@
-export const signup = data => {
+export const signup = credentials => {
   return (dispatch) => {
-    fetch('http://localhost:3000/api/v1/users', {
+    const userInfo = {
+      user: credentials
+    }
+    fetch('http://localhost:3000/api/v1/signup', {
+      credentials: 'include',
       headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
       },
       method: 'POST',
-      body: JSON.stringify(data)
+      body: JSON.stringify(userInfo)
     })
       .then(res => res.json())
       .then(user => {
         if (user.error) {
           alert(user.error)
         } else {
-          dispatch({type: 'ADD_USER', payload: user})
+          dispatch({type: 'SET_CURRENT_USER', user: user})
+          dispatch({type: 'RESET_SIGNUP_FORM'})
         }
       })
   }
