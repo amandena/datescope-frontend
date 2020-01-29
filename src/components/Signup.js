1c47da487d86986eaac218782e743592a65aa716
amandena <manda.panda91@yahoo.com>
2020-01-28 21:53:00 -0500

    refactored form to change it to a stateless component with mapStateToProps

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,106 +1,90 @@
 import React from 'react'
 import {connect} from 'react-redux'
-import {addUser} from '../actions/addUser'
+import {updateSignupForm} from '../actions/updateSignupForm'
+import {signup} from '../actions/signup'
 
-class Signup extends React.Component {
-  state = {
-    email: '',
-    password: '',
-    name: '',
-    age: '',
-    sign: '',
-    bio: '',
-    gender: '',
-    preference: '',
-    location: '',
-    instagram: ''
+const Signup = ({signupFormData, updateSignupForm, signup}) => {
+
+  const handleChange = event => {
+    const {name, value} = event.target
+    const updatedFormInfo = {
+      ...signupFormData,
+      [name]: value
+    }
+    updateSignupForm(updatedFormInfo)
   }
 
-  handleChange = event => {
-    this.setState({
-      [event.target.name]: event.target.value
-    })
-  }
-
-  handleSubmit = event => {
+  const handleSubmit = event => {
     event.preventDefault()
-    this.props.addUser(this.state)
-    this.setState({
-      email: '',
-      password: '',
-      name: '',
-      age: '',
-      sign: '',
-      bio: '',
-      gender: '',
-      preference: '',
-      location: '',
-      instagram: ''
-    })
+    signup(signupFormData)
   }
 
-  render() {
-    return(
-      <div>
-        <form onSubmit={this.handleSubmit}>
-          <label>Email: </label>
-          <input type='text' placeholder='Email' value={this.state.email} name='email' onChange={this.handleChange} /><br/>
+  return(
+    <div>
+      <form onSubmit={handleSubmit}>
+        <label>Email: </label>
+        <input type='text' placeholder='Email' value={signupFormData.email} name='email' onChange={handleChange} /><br/>
 
-          <label>Password: </label>
-          <input type='password' placeholder='Password' value={this.state.password} name='password' onChange={this.handleChange} /><br/>
+        <label>Password: </label>
+        <input type='password' placeholder='Password' value={signupFormData.password} name='password' onChange={handleChange} /><br/>
 
-          <label>Name: </label>
-          <input type='text' placeholder='Name' value={this.state.name} name='name' onChange={this.handleChange} /><br/>
+        <label>Name: </label>
+        <input type='text' placeholder='Name' value={signupFormData.name} name='name' onChange={handleChange} /><br/>
 
-          <label>Age: </label>
-          <input type='number' placeholder='Age' value={this.state.age} name='age' onChange={this.handleChange} /><br/>
+        <label>Age: </label>
+        <input type='number' placeholder='Age' value={signupFormData.age} name='age' onChange={handleChange} /><br/>
 
-          <label>Sign: </label>
-          <select value={this.state.sign} name='sign' onChange={this.handleChange}>
-            <option></option>
-            <option>aries</option>
-            <option>taurus</option>
-            <option>gemini</option>
-            <option>cancer</option>
-            <option>leo</option>
-            <option>virgo</option>
-            <option>libra</option>
-            <option>scorpio</option>
-            <option>sagittarius</option>
-            <option>capricorn</option>
-            <option>aquarius</option>
-            <option>pisces</option>
-          </select><br/>
+        <label>Sign: </label>
+        <select value={signupFormData.sign} name='sign' onChange={handleChange}>
+          <option></option>
+          <option>aries</option>
+          <option>taurus</option>
+          <option>gemini</option>
+          <option>cancer</option>
+          <option>leo</option>
+          <option>virgo</option>
+          <option>libra</option>
+          <option>scorpio</option>
+          <option>sagittarius</option>
+          <option>capricorn</option>
+          <option>aquarius</option>
+          <option>pisces</option>
+        </select><br/>
 
-          <label>Bio: </label>
-          <input type='text' placeholder='Bio' value={this.state.bio} name='bio' onChange={this.handleChange} /><br/>
+        <label>Bio: </label>
+        <input type='text' placeholder='Bio' value={signupFormData.bio} name='bio' onChange={handleChange} /><br/>
 
-          <label>Gender: </label>
-          <select value={this.state.gender} name='gender' onChange={this.handleChange}>
-            <option></option>
-            <option>male</option>
-            <option>female</option>
-          </select><br/>
+        <label>Gender: </label>
+        <select value={signupFormData.gender} name='gender' onChange={handleChange}>
+          <option></option>
+          <option>male</option>
+          <option>female</option>
+        </select><br/>
 
-          <label>Preference: </label>
-          <select value={this.state.preference} name='preference' onChange={this.handleChange}>
-            <option></option>
-            <option>male</option>
-            <option>female</option>
-            <option>both</option>
-          </select><br/>
+        <label>Preference: </label>
+        <select value={signupFormData.preference} name='preference' onChange={handleChange}>
+          <option></option>
+          <option>male</option>
+          <option>female</option>
+          <option>both</option>
+        </select><br/>
 
-        <label>Location: </label>
-          <input type='text' placeholder='Location' value={this.state.location} name='location' onChange={this.handleChange} /><br/>
+      <label>Location: </label>
+        <input type='text' placeholder='Location' value={signupFormData.location} name='location' onChange={handleChange} /><br/>
 
-          <label>Instagram: </label>
-          <input type='text' placeholder='Instagram' value={this.state.instagram} name='instagram' onChange={this.handleChange} /><br/>
+        <label>Instagram: </label>
+        <input type='text' placeholder='Instagram' value={signupFormData.instagram} name='instagram' onChange={handleChange} /><br/>
 
-          <input type='submit' value='Create Profile' />
-        </form>
-      </div>
-    )
+        <input type='submit' value='Create Profile' />
+      </form>
+    </div>
+  )
+}
+
+const mapStateToProps = state => {
+  return {
+    signupFormData: state.signupForm
   }
 }
 
-export default connect(null, {addUser})(Signup)
+export default connect(mapStateToProps, {updateSignupForm, signup})(Signup)
