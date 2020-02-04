31ec3e589e5a7b96648f81d1cf8a82b4d38d2d12
amandena <manda.panda91@yahoo.com>
2020-02-03 21:59:07 -0500

    re-added logout button to navbar

--- src/containers/NavBarContainer.js
+++ src/containers/NavBarContainer.js
@@ -1,22 +1,14 @@
 import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
-import Login from '../components/Login'
-import Logout from '../components/Logout'
 
 const NavBarContainer = ({currentUser}) => {
 
 
   return (
     <div className='navbar'>
-      <button>Login</button>
-      OR
-      <button>Sign Up</button>
-
-      <Link to='/'>Datescope</Link>
       <Link to='/users'>Profiles</Link>
-      {currentUser ? <strong>Welcome, {currentUser.name}!</strong> : ''}
-      {currentUser ? <Logout /> : <Login />}
+    {currentUser ? <strong>Welcome, {currentUser.name}! <Link to='/logout'>Logout</Link></strong> : ''}
     </div>
   )
 }
