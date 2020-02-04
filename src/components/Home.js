8333edaa9b2e4114addaf870091ed5e84a17989b
amandena <manda.panda91@yahoo.com>
2020-02-03 22:01:14 -0500

    created Home component with login and signup links

---
+++ src/components/Home.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+
+const Home = () => {
+  return (
+    <div>
+      <h3>Welcome, please <Link to='/login'>Log In</Link>.</h3>
+      <p>New to Datescope? Create account <Link to='/signup'>here</Link>.</p>
+    </div>
+  )
+}
+
+export default Home
