73db5fd0f544a2c22e3a6e2797f348dc9ee75565
amandena <manda.panda91@yahoo.com>
2020-02-06 23:45:44 -0500

    added withRouter and handleSubmit event handler to redirect to home page after logging out

--- src/components/Logout.js
+++ src/components/Logout.js
@@ -1,17 +1,23 @@
 import React from 'react'
 import {connect} from 'react-redux'
+import {withRouter} from 'react-router-dom'
 import {logoutUser} from '../actions/logoutUser'
 
 
-const Logout = ({logoutUser}) => {
+const Logout = ({logoutUser, history}) => {
+  const handleSubmit = event => {
+      event.preventDefault()
+      logoutUser()
+      history.push('/')
+    }
 
   return(
     <div>
-      <form onSubmit={logoutUser}>
+      <form onSubmit={handleSubmit}>
         <input type='submit' value='Logout' />
       </form>
     </div>
   )
 }
 
-export default connect(null, {logoutUser})(Logout)
+export default withRouter(connect(null, {logoutUser})(Logout))
