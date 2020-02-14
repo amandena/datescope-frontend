c657872dd1293d00ec1cfb22ee58bb69edfbfbc9
amandena <manda.panda91@yahoo.com>
2020-02-13 22:06:57 -0500

    fixed component

--- src/components/User.js
+++ src/components/User.js
@@ -1,19 +1,25 @@
 import React from 'react'
 import {Link} from 'react-router-dom'
-import MessagesContainer from '../containers/MessagesContainer'
+import {connect} from 'react-redux'
 
-const User = props => {
+const User = ({users, match}) => {
 
-  let user = props.users[props.match.params.id - 1]
+  let user = users.users[match.params.id]
 
   return(
     <div>
       <h2>{user ? user.name : null} - {user ? user.age : null} - {user ? user.sign : null}</h2>
       <p>{user ? user.bio : null}</p>
       <Link to={`/users/${user && user.id}/edit`}>Edit Profile</Link>
-      <MessagesContainer user={user} />
+      
     </div>
   )
 }
 
-export default User
+const mapStateToProps = state => {
+  return {
+    users: state.users
+  }
+}
+
+export default connect(mapStateToProps)(User)
