8dc32f476ebec4f148f0d9828139d4830a4634f7
amandena <manda.panda91@yahoo.com>
2020-03-02 14:37:50 -0500

    removed dispatch to clear messages

--- src/actions/logoutUser.js
+++ src/actions/logoutUser.js
@@ -1,7 +1,6 @@
 export const logoutUser = () => {
   return (dispatch) => {
     dispatch({type: 'CLEAR_CURRENT_USER'})
-    dispatch({type: 'CLEAR_MESSAGES'})
     fetch('http://localhost:3000/api/v1/logout', {
       credentials: 'include',
       method: 'DELETE'
