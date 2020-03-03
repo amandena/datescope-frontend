ba3d9f62f9698adf4737a5f444882dc74cdbc44e
amandena <manda.panda91@yahoo.com>
2020-03-03 16:18:34 -0500

    refactored link to edit profile to only show on current user's profile

--- src/components/User.js
+++ src/components/User.js
@@ -2,7 +2,7 @@ import React from 'react'
 import {Link} from 'react-router-dom'
 import {connect} from 'react-redux'
 
-const User = ({users, match}) => {
+const User = ({users, currentUser, match}) => {
 
   let user = users.users[match.params.id]
 
@@ -10,15 +10,15 @@ const User = ({users, match}) => {
     <div>
       <h2>{user ? user.name : null} - {user ? user.age : null} - {user ? user.sign : null}</h2>
       <p>{user ? user.bio : null}</p>
-      <Link to={`/users/${user && user.id}/edit`}>Edit Profile</Link>
-
+      {currentUser.id === user.id ? <Link to={`/users/${user && user.id}/edit`}>Edit Profile</Link> : ''}
     </div>
   )
 }
 
 const mapStateToProps = state => {
   return {
-    users: state.users
+    users: state.users,
+    currentUser: state.currentUser
   }
 }
 
