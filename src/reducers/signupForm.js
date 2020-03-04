4a7b985c8a89b835f1e8fda753544259e2253a2c
amandena <manda.panda91@yahoo.com>
2020-03-04 00:03:55 -0500

    added new cases for editing a user

--- src/reducers/signupForm.js
+++ src/reducers/signupForm.js
@@ -15,8 +15,17 @@ export default (state = initialState, action) => {
   switch (action.type) {
     case 'UPDATE_SIGNUP_FORM':
       return action.formData
+
     case 'RESET_SIGNUP_FORM':
       return initialState
+
+    case 'SET_FORM_DATA_FOR_EDIT_USER':
+      return action.userFormData
+
+    case 'EDIT_USER':
+      console.log("in edit user case, action is", action)
+      return state
+
     default:
       return state
   }
