3454a17b23062755206dfa8ae5b0572d4738d420
amandena <manda.panda91@yahoo.com>
2020-03-04 00:07:57 -0500

    created new component to wrap new user form

---
+++ src/components/NewUserFormWrapper.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import {connect} from 'react-redux'
+import {signup} from '../actions/signup'
+import Signup from './Signup'
+
+const NewUserFormWrapper = ({history, signup}) => {
+
+  const handleSubmit = (event, signupFormData, history) => {
+    event.preventDefault()
+    signup(signupFormData, history)
+  }
+
+  return (
+    <div>
+      <Signup history={history} handleSubmit={handleSubmit}/>
+    </div>
+  )
+}
+
+export default connect(null, {signup})(NewUserFormWrapper)
