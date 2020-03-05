5706f82e32ad790ba6a1e52bed7ea5bd159d8d5b
amandena <manda.panda91@yahoo.com>
2020-03-04 23:15:30 -0500

    renamed component to LoginSignup

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import NavBarContainer from './containers/NavBarContainer'
 import UsersContainer from './containers/UsersContainer'
 import NewUserFormWrapper from './components/NewUserFormWrapper'
 import Login from './components/Login'
-import Home from './components/Home'
+import LoginSignup from './components/LoginSignup'
 
 import {getCurrentUser} from './actions/getCurrentUser'
 
@@ -21,7 +21,7 @@ class App extends React.Component {
     const {currentUser} = this.props
     return(
       <div className="App">
-        {currentUser.id ? <NavBarContainer location={this.props.location}/> : <Home/>}
+        {currentUser.id ? <NavBarContainer location={this.props.location}/> : <LoginSignup/>}
         <Switch>
           <Route exact path='/signup' component={NewUserFormWrapper}/>
           <Route exact path='/login' component={Login}/>
