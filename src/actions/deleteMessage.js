25a4bbad80f0c1f41f75153e7608bffa102cd826
amandena <manda.panda91@yahoo.com>
2020-03-04 21:39:48 -0500

    added credentials and headers to fetch request

--- src/actions/deleteMessage.js
+++ src/actions/deleteMessage.js
@@ -1,7 +1,11 @@
 export const deleteMessage = (messageId, userId) => {
   return (dispatch) => {
     fetch(`http://localhost:3000/api/v1/users/${userId}/messages/${messageId}`, {
-      method: 'DELETE'
+      credentials: 'include',
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json'
+      }
     })
       .then(res => res.json())
       .then(user => dispatch({type: 'DELETE_MESSAGE', payload: user}))
