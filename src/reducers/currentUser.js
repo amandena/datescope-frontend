465f4511c916443754f348b10288a5347e11420f
amandena <manda.panda91@yahoo.com>
2020-03-07 20:35:44 -0500

    removed console.log

--- src/reducers/currentUser.js
+++ src/reducers/currentUser.js
@@ -1,10 +1,9 @@
 export default function currentUser(state = {messages: []}, action) {
   switch (action.type) {
-    case 'SET_CURRENT_USER':
+    case 'GET_CURRENT_USER':
       return action.user
 
     case 'UPDATE_CURRENT_USER_DATA':
-      console.log(action)
       return action.currentUserData
 
     case 'CLEAR_CURRENT_USER':
