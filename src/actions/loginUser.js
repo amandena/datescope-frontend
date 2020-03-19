4e7438bcc8a9ddc75436c3bf9e2e0911f7a970d2
amandena <manda.panda91@yahoo.com>
2020-03-18 22:03:13 -0400

    fixed action dispatch type to match reducer case

--- src/actions/loginUser.js
+++ src/actions/loginUser.js
@@ -11,10 +11,9 @@ export const loginUser = (credentials, history) => {
     })
       .then(res => res.json())
       .then(user => {
-        dispatch({type: 'SET_CURRENT_USER', user: user})
+        dispatch({type: 'GET_CURRENT_USER', user: user})
         dispatch({type: 'RESET_LOGIN_FORM'})
         history.push('/')
       })
-      .catch(console.log)
   }
 }
