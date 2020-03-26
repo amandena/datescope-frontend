3c201c3340c5fa3c283d095dcb73f10fa3e37e5a
amandena <manda.panda91@yahoo.com>
2020-03-26 02:21:03 -0400

    changed default state to an empty array and removed 2 messages cases

--- src/reducers/userReducer.js
+++ src/reducers/userReducer.js
@@ -1,40 +1,14 @@
-export default function userReducer(state = {users: []}, action) {
+export default function userReducer(state = [], action) {
   switch (action.type) {
     case 'FETCH_USERS':
-      return {users: action.payload}
+      return action.payload
 
     case 'ADD_USER':
-      return {...state, users: [...state.users, action.payload]}
+      return {...state, users: state.users.concat(action.payload)}
 
-    case 'ADD_MESSAGE':
-      let users = state.users.map(user => {
-        if (user.id === action.payload.id) {
-          return action.payload
-        } else {
-          return user
-        }
-      })
-      return {...state, users: users}
-
-      case 'DELETE_MESSAGE':
-        let usersD = state.users.map(user => {
-          if (user.id === action.payload.id) {
-            return action.payload
-          } else {
-            return user
-          }
-        })
-        return {...state, users: usersD}
-
-      case 'EDIT_USER':
-        let usersE = state.users.map(user => {
-          if (user.id === action.payload.id) {
-            return action.payload
-          } else {
-            return user
-          }
-        })
-        return {...state, users: usersE}
+    case 'EDIT_USER':
+      const user = (state !== action.payload ? action.payload : state)
+      return {...state, ...user}
 
     default:
       return state
