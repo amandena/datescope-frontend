fc4b7f3a66d2de47c596b3be6579be705a6ce96f
amandena <manda.panda91@yahoo.com>
2020-03-26 03:07:10 -0400

    refactored component into a class component and added currentUser to mapStateToProps

--- src/components/User.js
+++ src/components/User.js
@@ -2,22 +2,26 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 
-const User = ({users, match}) => {
+class User extends React.Component {
 
-  let user = users.users[match.params.id-1]
-
-  return(
-    <div className='user'>
-      <h2>{user ? user.name : null} - {user ? user.age : null} - {user ? user.sign : null}</h2>
-      <p>{user ? user.bio : null}</p>
-      <Link to={`/users/${user && user.id}/messenger`}>Send Message</Link>
-    </div>
-  )
+  render() {
+    const {users, currentUser} = this.props
+    const user = users[this.props.match.params.id-1]
+    
+    return(
+      <div className='user'>
+        <h2>{user ? user.name : null} - {user ? user.age : null} - {user ? user.sign : null}</h2>
+        <p>{user ? user.bio : null}</p>
+        <Link to={`/users/${currentUser.id}/messenger/${user && user.id}`}>Send Message</Link>
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = state => {
   return {
-    users: state.users
+    users: state.users,
+    currentUser: state.currentUser
   }
 }
 
